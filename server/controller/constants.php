4b3add40998749963021d6ae15b6e6c793e31031
kedyr <deniskedyr@gmail.com>
2014-03-01 00:52:28 +0300

    correcting db_namae

--- server/controller/constants.php
+++ server/controller/constants.php
@@ -6,6 +6,6 @@ class Constants
 	const MYSQL_HOST = "tunnel.pagodabox.com";
 	const MYSQL_USERNAME = "randa";
 	const MYSQL_PASSWORD = "o7eZYdJE";
-	const MYSQL_DATABASENAME = "agrofarm ";
+	const MYSQL_DATABASENAME = "agrofarm";
 }
 ?>
